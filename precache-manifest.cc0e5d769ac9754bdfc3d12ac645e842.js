492db754b4ba51c274de8266b4eed508dea746fe
Jesse Pinho <jesse@jessepinho.com>
2019-07-13 21:10:20 +0200

    Updates

---
+++ precache-manifest.cc0e5d769ac9754bdfc3d12ac645e842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557ad671f8cc5cda8645d134282453fc",
+    "url": "/checkbox-mosaic/index.html"
+  },
+  {
+    "revision": "9259db4a55e680b99cc0",
+    "url": "/checkbox-mosaic/static/css/main.db8e424e.chunk.css"
+  },
+  {
+    "revision": "edb83d6bb90e0a3359dd",
+    "url": "/checkbox-mosaic/static/js/2.c201882b.chunk.js"
+  },
+  {
+    "revision": "9259db4a55e680b99cc0",
+    "url": "/checkbox-mosaic/static/js/main.66c71986.chunk.js"
+  },
+  {
+    "revision": "9acfb3174e98537299c2",
+    "url": "/checkbox-mosaic/static/js/runtime~main.6715f622.js"
+  }
+]);

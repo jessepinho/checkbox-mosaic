36f0ee49b23d3757a28dcfacc6455cb94c47988d
Jesse Pinho <jesse@jessepinho.com>
2019-07-13 18:09:57 +0200

    chore: tweak file structure; apply Prettier styles

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const App: React.FC = () => <h1>Hello, world!</h1>
-
-export default App;
---
+++ src/components/App/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './styles.css'
+
+const App: React.FC = () => <h1>Hello, world!</h1>
+
+export default App

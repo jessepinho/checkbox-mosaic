ce59ad61d3163665885844a5f490dd5008135b9b
Jesse Pinho <jesse@jessepinho.com>
2019-07-13 20:06:30 +0200

    feat: control the checkboxes with React state

--- src/components/CheckboxGrid/index.tsx
+++ src/components/CheckboxGrid/index.tsx
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css'
 
 const DIMENSIONS = { width: 40, height: 40 }
 
-const CheckboxGrid: React.FC<{}> = props => (
-  <div className="CheckboxGrid">
-    {Array(DIMENSIONS.width)
-      .fill(null)
-      .map((_, yIndex) => {
-        return Array(DIMENSIONS.height)
-          .fill(null)
-          .map((_, xIndex) => {
-            return (
-              <input
-                type="checkbox"
-                key={`${xIndex},${yIndex}`}
-                className="CheckboxGrid__checkbox"
-              />
-            )
-          })
-      })}
-  </div>
-)
+interface Checked {
+  [coordinates: string]: boolean
+}
+
+const getCoordinates = (xIndex: number, yIndex: number): string =>
+  `${xIndex},${yIndex}`
+
+const CheckboxGrid: React.FC<{}> = props => {
+  const [checked, setChecked] = useState<Checked>({})
+
+  const toggleCheckbox = (coordinates: string) => {
+    setChecked(currentChecked => ({
+      ...currentChecked,
+      [coordinates]: !currentChecked[coordinates],
+    }))
+  }
+
+  return (
+    <div className="CheckboxGrid">
+      {Array(DIMENSIONS.width)
+        .fill(null)
+        .map((_, yIndex) => {
+          return Array(DIMENSIONS.height)
+            .fill(null)
+            .map((_, xIndex) => {
+              const coordinates = getCoordinates(xIndex, yIndex)
+              return (
+                <input
+                  type="checkbox"
+                  key={coordinates}
+                  className="CheckboxGrid__checkbox"
+                  checked={!!checked[coordinates]}
+                  onChange={() => toggleCheckbox(coordinates)}
+                />
+              )
+            })
+        })}
+    </div>
+  )
+}
 
 export default CheckboxGrid
